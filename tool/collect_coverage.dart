ef655571a6e7eef222cf29f244e4db9e1a6fbcfb
Ole Martin Gjersvik <olem@olem.org>
2014-06-01 21:57:28 +0200

    Addet coverage package.

---
+++ tool/collect_coverage.dart
@@ -0,0 +1,139 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:convert' show JSON;
+import 'dart:io';
+import 'package:args/args.dart';
+import 'package:coverage/src/devtools.dart';
+import 'package:coverage/src/util.dart';
+import 'package:http/http.dart' as http;
+
+Future<Map> getAllCoverage(Observatory observatory) {
+  return observatory.getIsolates()
+      .then((isolates) => isolates.map((i) => i.getCoverage()))
+      .then(Future.wait)
+      .then((responses) {
+        // flatten response lists
+        var allCoverage = responses.expand((it) => it).toList();
+        return {
+          'type': 'CodeCoverage',
+          'coverage': allCoverage,
+        };
+    });
+}
+
+Future resumeIsolates(Observatory observatory) {
+  return observatory.getIsolates()
+      .then((isolates) => isolates.map((i) => i.resume()))
+      .then(Future.wait);
+}
+
+Future<Observatory> connect(String host, String port) {
+  return http.get('http://$host:$port/json').then((resp) {
+    var json = JSON.decode(resp.body);
+    if (json is List) {
+      return Observatory.connectOverDevtools(host, port);
+    }
+    return Observatory.connect(host, port);
+  });
+}
+
+Future waitIsolatesPaused(Observatory observatory) {
+  allPaused() => observatory.getIsolates()
+      .then((isolates) => isolates.every((i) => i.paused))
+      .then((paused) => paused ? paused : new Future.error(paused));
+  return retry(allPaused, RETRY_INTERVAL);
+}
+
+const RETRY_INTERVAL = const Duration(milliseconds: 200);
+
+void main(List<String> arguments) {
+  var options = parseArgs(arguments);
+  onTimeout() {
+    var timeout = options.timeout.inSeconds;
+    print('Failed to collect coverage within ${timeout}s');
+    exit(1);
+  }
+  Future connected =
+      retry(() => connect(options.host, options.port), RETRY_INTERVAL);
+  if (options.timeout != null) {
+    connected.timeout(options.timeout, onTimeout: onTimeout);
+  }
+  connected.then((observatory) {
+    Future ready = options.waitPaused
+        ? waitIsolatesPaused(observatory)
+        : new Future.value();
+    if (options.timeout != null) {
+      ready.timeout(options.timeout, onTimeout: onTimeout);
+    }
+    return ready.then((_) => getAllCoverage(observatory))
+        .then(JSON.encode)
+        .then(options.out.write)
+        .then((_) => options.out.close())
+        .then((_) => options.resume ? resumeIsolates(observatory) : null)
+        .then((_) => observatory.close());
+  });
+}
+
+class Options {
+  final String host;
+  final String port;
+  final IOSink out;
+  final Duration timeout;
+  final bool waitPaused;
+  final bool resume;
+  Options(this.host, this.port, this.out, this.timeout, this.waitPaused,
+      this.resume);
+}
+
+Options parseArgs(List<String> arguments) {
+  var parser = new ArgParser();
+
+  parser.addOption('host', abbr: 'H', defaultsTo: '127.0.0.1',
+      help: 'remote VM host');
+  parser.addOption('port', abbr: 'p', help: 'remote VM port');
+  parser.addOption('out', abbr: 'o', defaultsTo: 'stdout',
+      help: 'output: may be file or stdout');
+  parser.addOption('connect-timeout', abbr: 't',
+      help: 'connect timeout in seconds');
+  parser.addFlag('wait-paused', abbr: 'w', defaultsTo: false,
+      help: 'wait for all isolates to be paused before collecting coverage');
+  parser.addFlag('resume-isolates', abbr: 'r', defaultsTo: false,
+      help: 'resume all isolates on exit');
+  parser.addFlag('help', abbr: 'h', negatable: false,
+      help: 'show this help');
+  var args = parser.parse(arguments);
+
+  printUsage() {
+    print('Usage: dart collect_coverage.dart --port=NNNN [OPTION...]\n');
+    print(parser.getUsage());
+  }
+
+  fail(message) {
+    print('Error: $message\n');
+    printUsage();
+    exit(1);
+  }
+
+  if (args['help']) {
+    printUsage();
+    exit(0);
+  }
+
+  if (args['port'] == null) fail('port not specified');
+
+  var out;
+  if (args['out'] == 'stdout') {
+    out = stdout;
+  } else {
+    var outfile = new File(args['out'])
+        ..createSync(recursive: true);
+    out = outfile.openWrite();
+  }
+  var timeout = (args['connect-timeout'] == null) ? null
+      : new Duration(seconds: int.parse(args['connect-timeout']));
+  return new Options(args['host'], args['port'], out, timeout,
+      args['wait-paused'], args['resume-isolates']);
+}
---
+++ tool/format_coverage.dart
@@ -0,0 +1,277 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+
+import 'package:args/args.dart';
+import 'package:coverage/coverage.dart';
+import 'package:path/path.dart';
+
+
+/// Given an absolute path absPath, this function returns a [List] of files
+/// are contained by it if it is a directory, or a [List] containing the file if
+/// it is a file.
+List filesToProcess(String absPath) {
+  var filePattern = new RegExp(r'^dart-cov-\d+-\d+.json$');
+  if (FileSystemEntity.isDirectorySync(absPath)) {
+    return new Directory(absPath).listSync(recursive: true)
+        .where((entity) => entity is File &&
+            filePattern.hasMatch(basename(entity.path)))
+        .toList();
+  }
+
+  return [new File(absPath)];
+}
+
+worker(WorkMessage msg) {
+  final start = new DateTime.now().millisecondsSinceEpoch;
+
+  List files = msg.files;
+  var resolver = new Resolver(packageRoot: msg.pkgRoot, sdkRoot: msg.sdkRoot);
+  var workerHitmap = {};
+  files.forEach((File fileEntry) {
+    // Read file sync, as it only contains 1 object.
+    String contents = fileEntry.readAsStringSync();
+    if (contents.length > 0) {
+      mergeHitmaps(createHitmap(contents, resolver), workerHitmap);
+    }
+  });
+
+  if (msg.verbose) {
+    final end = new DateTime.now().millisecondsSinceEpoch;
+    print('${msg.workerName}: Finished processing ${files.length} files. '
+          'Took ${end - start} ms.');
+  }
+
+  msg.replyPort.send(new ResultMessage(workerHitmap, resolver.failed));
+}
+
+class WorkMessage {
+  final String workerName;
+  final String sdkRoot;
+  final String pkgRoot;
+  final List files;
+  final SendPort replyPort;
+  final bool verbose;
+  WorkMessage(this.workerName, this.pkgRoot, this.sdkRoot, this.files,
+      this.replyPort, this.verbose);
+}
+
+class ResultMessage {
+  final hitmap;
+  final failedResolves;
+  ResultMessage(this.hitmap, this.failedResolves);
+}
+
+List<List> split(List list, int nBuckets) {
+  var buckets = new List(nBuckets);
+  var bucketSize = list.length ~/ nBuckets;
+  var leftover = list.length % nBuckets;
+  var taken = 0;
+  var start = 0;
+  for (int i = 0; i < nBuckets; i++) {
+    var end = (i < leftover) ? (start + bucketSize + 1) : (start + bucketSize);
+    buckets[i] = list.sublist(start, end);
+    taken += buckets[i].length;
+    start = end;
+  }
+  if (taken != list.length) throw 'Error splitting';
+  return buckets;
+}
+
+Future<ResultMessage> spawnWorker(name, pkgRoot, sdkRoot, files, verbose) {
+  RawReceivePort port = new RawReceivePort();
+  var completer = new Completer();
+  port.handler = ((ResultMessage msg) {
+    completer.complete(msg);
+    port.close();
+  });
+  var msg = new WorkMessage(name, pkgRoot, sdkRoot, files, port.sendPort, verbose);
+  Isolate.spawn(worker, msg);
+  return completer.future;
+}
+
+/// [Environment] stores gathered arguments information.
+class Environment {
+  String sdkRoot;
+  String pkgRoot;
+  String input;
+  IOSink output;
+  int workers;
+  bool prettyPrint;
+  bool lcov;
+  bool expectMarkers;
+  bool verbose;
+}
+
+main(List<String> arguments) {
+  final env = parseArgs(arguments);
+
+  List files = filesToProcess(env.input);
+
+  List failedResolves = [];
+  List failedLoads = [];
+  Map globalHitmap = {};
+  int start = new DateTime.now().millisecondsSinceEpoch;
+
+  if (env.verbose) {
+    print('Environment:');
+    print('  # files: ${files.length}');
+    print('  # workers: ${env.workers}');
+    print('  sdk-root: ${env.sdkRoot}');
+    print('  package-root: ${env.pkgRoot}');
+  }
+
+  // Create workers.
+  int workerId = 0;
+  var results = split(files, env.workers).map((workerFiles) {
+    var result = spawnWorker('Worker ${workerId++}', env.pkgRoot, env.sdkRoot,
+        workerFiles, env.verbose);
+    return result.then((ResultMessage message) {
+      mergeHitmaps(message.hitmap, globalHitmap);
+      failedResolves.addAll(message.failedResolves);
+    });
+  });
+
+  Future.wait(results).then((ignore) {
+    // All workers are done. Process the data.
+    if (env.verbose) {
+      final end = new DateTime.now().millisecondsSinceEpoch;
+      print('Done creating a global hitmap. Took ${end - start} ms.');
+    }
+
+    Future out;
+    if (env.prettyPrint) {
+      out = prettyPrint(globalHitmap, failedLoads, env.output);
+    } else if (env.lcov) {
+      out = lcov(globalHitmap, env.output);
+    }
+
+    out.then((_) {
+      env.output.close().then((_) {
+        if (env.verbose) {
+          final end = new DateTime.now().millisecondsSinceEpoch;
+          print('Done flushing output. Took ${end - start} ms.');
+        }
+      });
+
+      if (env.verbose) {
+        if (failedResolves.length > 0) {
+          print('Failed to resolve:');
+          failedResolves.toSet().forEach((e) {
+            print('  ${e}');
+          });
+        }
+        if (failedLoads.length > 0) {
+          print('Failed to load:');
+          failedLoads.toSet().forEach((e) {
+            print('  ${e}');
+          });
+        }
+      }
+    });
+  });
+}
+
+/// Checks the validity of the provided arguments. Does not initialize actual
+/// processing.
+parseArgs(List<String> arguments) {
+  final env = new Environment();
+  var parser = new ArgParser();
+
+  parser.addOption('sdk-root', abbr: 's',
+      help: 'path to the SDK root');
+  parser.addOption('package-root', abbr: 'p',
+      help: 'path to the package root');
+  parser.addOption('in', abbr: 'i',
+      help: 'input(s): may be file or directory');
+  parser.addOption('out', abbr: 'o', defaultsTo: 'stdout',
+      help: 'output: may be file or stdout');
+  parser.addOption('workers', abbr: 'j', defaultsTo: '1',
+      help: 'number of workers');
+  parser.addFlag('pretty-print', abbr: 'r', negatable: false,
+      help: 'convert coverage data to pretty print format');
+  parser.addFlag('lcov', abbr :'l', negatable: false,
+      help: 'convert coverage data to lcov format');
+  parser.addFlag('verbose', abbr :'v', negatable: false,
+      help: 'verbose output');
+  parser.addFlag('help', abbr: 'h', negatable: false,
+      help: 'show this help');
+
+  var args = parser.parse(arguments);
+
+  printUsage() {
+    print('Usage: dart format_coverage.dart [OPTION...]\n');
+    print(parser.getUsage());
+  }
+
+  fail(String msg) {
+    print('\n$msg\n');
+    printUsage();
+    exit(1);
+  }
+
+  if (args['help']) {
+    printUsage();
+    exit(0);
+  }
+
+  env.sdkRoot = args['sdk-root'];
+  if (env.sdkRoot == null) {
+    if (Platform.environment.containsKey('DART_SDK')) {
+      env.sdkRoot =
+        join(absolute(normalize(Platform.environment['DART_SDK'])), 'lib');
+    }
+  } else {
+    env.sdkRoot = join(absolute(normalize(env.sdkRoot)), 'lib');
+  }
+  if ((env.sdkRoot != null) && !FileSystemEntity.isDirectorySync(env.sdkRoot)) {
+    fail('Provided SDK root "${args["sdk-root"]}" is not a valid SDK '
+         'top-level directory');
+  }
+
+  env.pkgRoot = args['package-root'];
+  if (env.pkgRoot != null) {
+    env.pkgRoot = absolute(normalize(args['package-root']));
+    if (!FileSystemEntity.isDirectorySync(env.pkgRoot)) {
+      fail('Provided package root "${args["package-root"]}" is not directory.');
+    }
+  }
+
+  if (args['in'] == null) fail('No input files given.');
+  env.input = absolute(normalize(args['in']));
+  if (!FileSystemEntity.isDirectorySync(env.input) &&
+      !FileSystemEntity.isFileSync(env.input)) {
+    fail('Provided input "${args["in"]}" is neither a directory nor a file.');
+  }
+
+  if (args['out'] == 'stdout') {
+    env.output = stdout;
+  } else {
+    var outpath = absolute(normalize(args['out']));
+    var outfile = new File(outpath)
+        ..createSync(recursive: true);
+    env.output = outfile.openWrite();
+  }
+
+  env.lcov = args['lcov'];
+  if (args['pretty-print'] && env.lcov) {
+    fail('Choose one of pretty-print or lcov output');
+  }
+  if (!env.lcov) {
+    // Use pretty-print either explicitly or by default.
+    env.prettyPrint = true;
+  }
+
+  try {
+    env.workers = int.parse('${args["workers"]}');
+  } catch (e) {
+    fail('Invalid worker count: $e');
+  }
+
+  env.verbose = args['verbose'];
+  return env;
+}

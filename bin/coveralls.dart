97cf9742c060d225e33b1637e17a04590e3351fe
Ole Martin Gjersvik <olem@olem.org>
2014-06-02 14:52:48 +0200

    List only files inside project.

--- bin/coveralls.dart
+++ bin/coveralls.dart
@@ -5,6 +5,7 @@ import 'package:dartcoveralls/dartcoveralls.dart';
 
 main(){
   var coveralls = new Coveralls();
+  coveralls.root = '..';
   var file = new File('../cov.json');
   coveralls.addCoverage(JSON.decode(file.readAsStringSync()));
 }
--- lib/dartcoveralls.dart
+++ lib/dartcoveralls.dart
@@ -1,12 +1,23 @@
 library dartcoveralls;
 
+import 'package:path/path.dart' as path;
+
 class Coveralls{
+  String root = '.';
+  
   Map _coverage = {};
   
   addCoverage(Map data){
     var coverage = data['coverage'];
     coverage.forEach((Map file){
-      print(file['source']);
+      String source = file['source'];
+      if(!source.startsWith('file://')){
+        return;
+      }
+      source = path.fromUri(source);
+      if(path.isWithin(root,source)){
+        print(path.relative(source,from: root));
+      }
     });
   }
 }

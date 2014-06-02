d5651cb63f8597fc5ef01fab5c3a288336dffb7d
Ole Martin Gjersvik <olem@olem.org>
2014-06-02 14:01:26 +0200

    A better test script

--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,32 @@
 // Comment shuld not be counted.
-dud() => print('Will not run.');
-main() => print('Will run.');
+dud(){
+  // Comment shuld not be counted.
+  print('Will not run.');
+  print('Will not run.');
+}
+
+run1(){
+  print('Run 1/1');
+}
+
+run5(){
+  for(int i = 1; i < 6; i +=1){
+    print('Run $i/5');
+  }
+}
+
+run10(){
+  for(int i = 1; i < 11; i +=1){
+    print('Run $i/10');
+  }
+}
+
+main(){
+  print('Start');
+  run1();
+  run5();
+  run10();
+}
+
+// Comment shuld not be counted.
+// Comment shuld not be counted.

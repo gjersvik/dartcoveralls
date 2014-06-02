c5dfd45d0e77265b6355ce3be20d1b0694bc96ec
Ole Martin Gjersvik <olem@olem.org>
2014-06-02 15:14:44 +0200

    Normalice hits.

--- lib/dartcoveralls.dart
+++ lib/dartcoveralls.dart
@@ -1,5 +1,7 @@
 library dartcoveralls;
 
+import 'dart:math';
+
 import 'package:path/path.dart' as path;
 
 class Coveralls{
@@ -17,7 +19,27 @@ class Coveralls{
       source = path.fromUri(source);
       if(path.isWithin(root,source)){
         print(path.relative(source,from: root));
+        print(_toCoverallLineFormat(file['hits']));
       }
     });
+  }
+  
+  List<int> _toCoverallLineFormat(List cov){
+    List out = [];
+    for(int i = 0; i < cov.length; i += 2){
+      var line = cov[i];
+      var hits = cov[i+1];
+      // test length
+      if(out.length < line){
+        out.length = line;
+      }
+      
+      if(out[line-1] == null){
+        out[line-1] = hits;
+      }else{
+        out[line-1] = max(out[line-1], hits);
+      }
+    }
+    return out;
   }
 }

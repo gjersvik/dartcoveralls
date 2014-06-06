65b681d0441fe576d81373a0575a97808da05157
Ole Martin Gjersvik <olem@olem.org>
2014-06-06 16:12:00 +0200

    Removed first test.

--- test/unit.dart
+++ test/unit.dart
@@ -1,14 +1,11 @@
 library dartcoveralls_unit;
 
 import 'package:unittest/unittest.dart';
-import 'package:unittest/vm_config.dart';
 
 import 'package:dartcoveralls/dartcoveralls.dart';
 
 part 'src/coveralls.dart';
 
 main(){
-  //useVMConfiguration();
-  
   testCoveralls();
 }
